f84aa9c6d7358dc95c050ecfb1108d958ea84164
Desiree Mendes <desireefrost@live.ca>
2022-11-04 01:48:49 -0400

    adjust buttons

--- basketball-scoreboard/index.js
+++ basketball-scoreboard/index.js
@@ -2,6 +2,10 @@ let homeScoreBtnOne = document.getElementById("add-1")
 let homeStoreEl = document.getElementById("home-score")
 let homeScore = 0
 
+let guestScoreBtnOne = document.getElementById("add-1")
+let guestStoreEl = document.getElementById("guest-score")
+let guestScore = 0
+
 add1 = () => {
     // console.log("button clicked")
     homeScore += 1
